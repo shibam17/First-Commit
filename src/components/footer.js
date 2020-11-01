8a87a5328a09273e937fd2293a77cc393eb3b1b2
soulhunter10737 <shibam.dipu@gmail.com>
2020-11-01 12:28:53 +0530

    added

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = ({ author, profile }) => (
+    <footer>
+        Made with 
+        
+        <i className="em em-heart" style={{width: '15px', height: '15px'}}></i>
+        <a href="https://github.com/shibam17"> @Shibam</a>
+    </footer>
+)
+
+export default Footer
